9acd976379d45bfe30f5e241971c119d960494e9
Jonas Persson <jonas.persson111@gmail.com>
2026-09-24 15:54:33 +0200

    updated index with links

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: true }}>
-      <Stack.Screen name="index" options={{ title: "Start" }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: "Country Challenge" }} />
       <Stack.Screen name="profile" options={{ title: "Profile" }} />
       <Stack.Screen name="country/[id]" />
       <Stack.Screen name="region/[id]" />
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,54 +1,62 @@
+import { usePlayerProgress } from "@/data/usePlayerProgress";
 import { Link } from "expo-router";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const { data } = usePlayerProgress();
+
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-      <Pressable>
-        <Link
-          href={{
-            pathname: "/country/[id]",
-            params: { id: "123" },
-          }}
-        >
-          country123
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link
-          href={{
-            pathname: "/region/[id]",
-            params: { id: "123" },
-          }}
-        >
-          region123
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link
-          href={{
-            pathname: "/game/[countryid]",
-            params: { countryid: "123" },
-          }}
-        >
-          game123
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link
-          href={{
-            pathname: "/result/[countryid]",
-            params: { countryid: "123" },
-          }}
-        >
-          result123
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link href="/profile">profile</Link>
-      </Pressable>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Start screen</Text>
+        <Text>{data.xp}</Text>
+        <Text>{data.completedCountries}</Text>
+        <Pressable>
+          <Link
+            href={{
+              pathname: "/country/[id]",
+              params: { id: "123" },
+            }}
+          >
+            country123
+          </Link>
+        </Pressable>
+        <Pressable>
+          <Link
+            href={{
+              pathname: "/region/[id]",
+              params: { id: "123" },
+            }}
+          >
+            region123
+          </Link>
+        </Pressable>
+        <Pressable>
+          <Link
+            href={{
+              pathname: "/game/[countryid]",
+              params: { countryid: "123" },
+            }}
+          >
+            game123
+          </Link>
+        </Pressable>
+        <Pressable>
+          <Link
+            href={{
+              pathname: "/result/[countryid]",
+              params: { countryid: "123" },
+            }}
+          >
+            result123
+          </Link>
+        </Pressable>
+        <Pressable>
+          <Link href="/profile">profile</Link>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
