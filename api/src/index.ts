17e1a90cab57ca07625358000bfbc108352e3c43
Jonas Persson <jonas.persson111@gmail.com>
2026-09-24 16:58:53 +0200

    added cors to api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,22 +1,28 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { regions } from './routes/regions.js'
-import { countries } from './routes/countries.js'
-import { questions } from './routes/questions.js'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { countries } from "./routes/countries.js";
+import { questions } from "./routes/questions.js";
+import { regions } from "./routes/regions.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.json({ message: 'Country Challenge API' })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Country Challenge API" });
+});
 
-app.route('/regions', regions)
-app.route('/countries', countries)
-app.route('/questions', questions)
+app.route("/regions", regions);
+app.route("/countries", countries);
+app.route("/questions", questions);
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.use("*", cors());
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
